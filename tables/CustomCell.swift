95b4ec5246b382f030e96e7e5eee165fd529a79e
mononomorapeli <minkymorapeli@gmail.com>
2023-05-23 16:26:45 +0200

    first commit

---
+++ tables/CustomCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCell.swift
+//  tables
+//
+//  Created by DA MAC M1 122 on 2023/05/23.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
